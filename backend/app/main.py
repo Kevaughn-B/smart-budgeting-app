93bf7f71f8cb7d7e82446e320c948f541a6c8b98
Kevaughn-B <k.dorianbenjamin@gmail.com>
2026-04-21 13:24:50 -0500

    Issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,29 @@
-from app.core.config import settings
-import os
-from app.api.users import router as users_router
 from fastapi import FastAPI
 from dotenv import load_dotenv
+
+from app.core.config import settings
 from app.api.health import router as health_router
-#Temporary imports to create DB tables
+from app.api.users import router as users_router
+from app.api.auth import router as auth_router
+
+# Temporary DB setup
 from app.db.session import engine
 from app.db.base import Base
 
-#Test code to create tables
-
 load_dotenv()
 
+# Create tables
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
-    title=settings.APP_NAME, 
-    version=settings.APP_VERSION)
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION
+)
 
 app.include_router(health_router)
 app.include_router(users_router)
+app.include_router(auth_router)
+
 
 @app.get("/")
 def root():
