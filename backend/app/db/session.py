5ebbd88b418faca4ed7759005f235ab67cbdec86
Kevaughn-B <k.dorianbenjamin@gmail.com>
2026-04-23 22:46:53 -0500

    Checkpoint 4.5

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings  # ✅ ADD THIS
+from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL  # ✅ USE ENV
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
